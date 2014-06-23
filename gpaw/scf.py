2230269c15fa2ea09c82e72cd811086868961ea4
rbw <robert.warmbier@wits.ac.za>
2014-06-23 12:19:27 +0200

    Deleted too much... upps

--- gpaw/scf.py
+++ gpaw/scf.py
@@ -44,6 +44,7 @@ class SCFLoop:
         
         for iter in range(1, self.maxiter + 1):
             wfs.eigensolver.iterate(hamiltonian, wfs)
+            occupations.calculate(wfs)
 
             energy = hamiltonian.get_energy(occupations)
             self.energies.append(energy)
